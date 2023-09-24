379e6a4b72e2ebf76ec49d88dac301554ac3d4b8
Tobjoern <Tobi11111@gmx.de>
2023-09-24 19:48:56 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+export * from "./variables";
+export * from "./types";
+export * from "./classes";
+export * from "./enums";
+export * from "./reexports";
+export * from "./showcase";
+export * from "./reactComponents";
+export * from "./internals";
+
+/**
+ * This is the doc comment for thing1/functions.ts
+ *
+ * Specify this is a module comment and rename it to thing1:
+ * @module thing1
+ */
+export * from "./thing1/functions";
